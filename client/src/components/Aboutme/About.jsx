bde1e20737b3e2fe01c6ca5837214faefe8116e4
toum21 <pey2moo1@gmail.com>
2024-01-12 14:41:32 -0800

    added animation to play behind components on the homepage

--- client/src/components/Aboutme/About.jsx
+++ client/src/components/Aboutme/About.jsx
@@ -1,24 +1,23 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLinkedin, faGithub, faDiscord } from '@fortawesome/free-brands-svg-icons'
+import avatar from '../../assets/cartoon_LOAF-ezgif.com-webp-to-jpg-converter.jpg'
 
 function About() {
     return (
-        <div className="container d-flex align-items-center justify-content-center mt-5">
-            <div className="text-center">
-                <h1>Peyton Touma</h1>
-                <p>Full-Stack Web Developer and Software Engineer</p>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia cumque, incidunt possimus placeat sit dolorum praesentium aperiam laudantium unde reprehenderit distinctio dicta nobis dolorem voluptate velit perspiciatis quibusdam iste veritatis!
-                </p>
+        <div className="container text-center mt-5">
+            <h1>Peyton Touma</h1>
+            <p>Full-Stack Web Developer and Software Engineer</p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia cumque, incidunt possimus placeat sit dolorum praesentium aperiam laudantium unde reprehenderit distinctio dicta nobis dolorem voluptate velit perspiciatis quibusdam iste veritatis!
+            </p>
 
-                <img src="" alt="avatar image" />
-
-                <div className="mt-3"> {/* Add margin top for spacing */}
-                    <FontAwesomeIcon icon={faLinkedin} className="fa-2x mx-1" /> {/* Use fa-3x for larger size and mx-2 for horizontal margin */}
-                    <FontAwesomeIcon icon={faGithub} className="fa-2x mx-1" />
-                    <FontAwesomeIcon icon={faDiscord} className="fa-2x mx-1" />
-                </div>
+            <div className="mt-3">
+                <FontAwesomeIcon icon={faLinkedin} className="fa-2x mx-1" />
+                <FontAwesomeIcon icon={faGithub} className="fa-2x mx-1" />
+                <FontAwesomeIcon icon={faDiscord} className="fa-2x mx-1" />
             </div>
+
+            <img src={avatar} alt="avatar image" className="rounded-circle img-fluid mt-3" style={{ width: '200px', height: '200px' }} />
         </div>
     );
 }
