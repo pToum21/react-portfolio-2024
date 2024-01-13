e3862c1a07e3ddbd9f1a9d85c11e34243a3d1855
toum21 <pey2moo1@gmail.com>
2024-01-12 21:31:58 -0800

    updated the card to fit better and added the image as well

--- client/src/components/Portfolio/Portfolio.jsx
+++ client/src/components/Portfolio/Portfolio.jsx
@@ -1,18 +1,21 @@
+import React from 'react';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 
+import anyyesImage from '../../assets/anyyes.png';
+
 function Portfolio() {
     return (
-        <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="" />
+        <Card style={{ width: '25rem' }}>
+            <Card.Img variant="top" src={anyyesImage} style={{ height: '15rem', objectFit: 'cover' }} />
             <Card.Body>
                 <Card.Title>AnyYes</Card.Title>
-                <Card.Text>
+                <Card.Text style={{ fontSize: '0.85rem' }}>
                     This application is a full stack e-commerce site that allows the user to buy and sell retro gaming consoles and games.
                 </Card.Text>
             </Card.Body>
             <ListGroup className="list-group-flush">
-                <ListGroup.Item>
+                <ListGroup.Item style={{ fontSize: '0.8rem' }}>
                     <h5>Stack Used:</h5>
                     HTML, CSS (Bulma), Handlebars (with Express Session and Express Handlebars), JavaScript, Node.js, Express.js, MySQL (using mySQL2), Sequelize, Multer (for image upload), Blob storage, Stripe (for payments), bcrypt (for password hashing), dotenv, Config folder, Seeds, Splidejs (for image carousel).
                 </ListGroup.Item>
