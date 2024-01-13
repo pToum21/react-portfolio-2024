4d3427f91db134f89ab344c2fbcf5b1e67f7d879
toum21 <pey2moo1@gmail.com>
2024-01-13 14:35:14 -0800

    updated the portfolioi section to not be in a card anymore, going for an alternating style down the page

--- client/src/components/Portfolio/Portfolio.jsx
+++ client/src/components/Portfolio/Portfolio.jsx
@@ -1,31 +1,32 @@
 import React from 'react';
-import Card from 'react-bootstrap/Card';
-import ListGroup from 'react-bootstrap/ListGroup';
+import { Container, Row, Col } from 'react-bootstrap';
+import anyyes from '../../assets/anyyes.png';
 
-import anyyesImage from '../../assets/anyyes.png';
 
-function Portfolio() {
+const YourComponent = () => {
     return (
-        <Card className='mt-5' style={{ width: '25rem', background: '#1a1a1a', color: 'white'  }}>
-            <Card.Img variant="top" src={anyyesImage} style={{ height: '15rem', objectFit: 'cover'}} />
-            <Card.Body>
-                <Card.Title>AnyYes</Card.Title>
-                <Card.Text style={{ fontSize: '0.85rem' }}>
-                    This application is a full stack e-commerce site that allows the user to buy and sell retro gaming consoles and games.
-                </Card.Text>
-            </Card.Body>
-            <ListGroup className="list-group-flush">
-                <ListGroup.Item style={{ fontSize: '0.8rem', background: '#1a1a1a', color: 'white' }}>
-                    <h5>Stack Used:</h5>
-                    HTML, CSS (Bulma), Handlebars (with Express Session and Express Handlebars), JavaScript, Node.js, Express.js, MySQL (using mySQL2), Sequelize, Multer (for image upload), Blob storage, Stripe (for payments), bcrypt (for password hashing), dotenv, Config folder, Seeds, Splidejs (for image carousel).
-                </ListGroup.Item>
-            </ListGroup>
-            <Card.Body>
-                <Card.Link href="https://anyyes-3bf9b8d1cf29.herokuapp.com/">Deployed Demo</Card.Link>
-                <Card.Link href="https://github.com/pToum21/anyYes">GitHub Link</Card.Link>
-            </Card.Body>
-        </Card>
+        <Container>
+            <Row>
+                <Col xs={12} md={6}>
+                    <img src={anyyes} alt="Your Image" className="img-fluid" />
+                </Col>
+                <Col xs={12} md={6}>
+                    <h3>AnyYes</h3>
+                    <p>
+                        This application is a full stack e-commerce site that allows the user to buy and sell retro gaming consoles and games.
+                    </p>
+                    <div style={{ fontSize: '0.8rem', color: 'white' }}>
+                        <h5>Stack Used:</h5>
+                        HTML, CSS (Bulma), Handlebars (with Express Session and Express Handlebars), JavaScript, Node.js, Express.js, MySQL (using mySQL2), Sequelize, Multer (for image upload), Blob storage, Stripe (for payments), bcrypt (for password hashing), dotenv, Config folder, Seeds, Splidejs (for image carousel).
+                    </div>
+                    <div>
+                        <a href="https://anyyes-3bf9b8d1cf29.herokuapp.com/">Deployed Demo</a>
+                        <a href="https://github.com/pToum21/anyYes">GitHub Link</a>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
     );
-}
+};
 
-export default Portfolio;
+export default YourComponent;
