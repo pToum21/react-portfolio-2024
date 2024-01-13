21d7b076bb24fb7805be1f0c1030f88336f42b00
toum21 <pey2moo1@gmail.com>
2024-01-12 21:25:51 -0800

    updated the text inside the card element

--- client/src/components/Portfolio/Portfolio.jsx
+++ client/src/components/Portfolio/Portfolio.jsx
@@ -4,22 +4,22 @@ import ListGroup from 'react-bootstrap/ListGroup';
 function Portfolio() {
     return (
         <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+            <Card.Img variant="top" src="" />
             <Card.Body>
-                <Card.Title>Card Title</Card.Title>
+                <Card.Title>AnyYes</Card.Title>
                 <Card.Text>
-                    Some quick example text to build on the card title and make up the
-                    bulk of the card's content.
+                    This application is a full stack e-commerce site that allows the user to buy and sell retro gaming consoles and games.
                 </Card.Text>
             </Card.Body>
             <ListGroup className="list-group-flush">
-                <ListGroup.Item>Cras justo odio</ListGroup.Item>
-                <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-                <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+                <ListGroup.Item>
+                    <h5>Stack Used:</h5>
+                    HTML, CSS (Bulma), Handlebars (with Express Session and Express Handlebars), JavaScript, Node.js, Express.js, MySQL (using mySQL2), Sequelize, Multer (for image upload), Blob storage, Stripe (for payments), bcrypt (for password hashing), dotenv, Config folder, Seeds, Splidejs (for image carousel).
+                </ListGroup.Item>
             </ListGroup>
             <Card.Body>
-                <Card.Link href="#">Card Link</Card.Link>
-                <Card.Link href="#">Another Link</Card.Link>
+                <Card.Link href="https://anyyes-3bf9b8d1cf29.herokuapp.com/">Deployed Demo</Card.Link>
+                <Card.Link href="https://github.com/pToum21/anyYes">GitHub Link</Card.Link>
             </Card.Body>
         </Card>
     );
