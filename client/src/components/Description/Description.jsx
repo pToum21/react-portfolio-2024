bf41ec623229723273017d7846340e1f8be69bb1
toum21 <pey2moo1@gmail.com>
2024-01-12 21:08:50 -0800

    added more logos to my tech stack

--- client/src/components/Description/Description.jsx
+++ client/src/components/Description/Description.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { faJs, faHtml5, faCss3, faReact, faNode, faBootstrap, faStripe, faFigma, faGithub } from '@fortawesome/free-brands-svg-icons';
-import { faDatabase } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './description.css';
 import portrait from '../../assets/IMG_2452.jpg';
@@ -18,12 +17,24 @@ function Description() {
                             <FontAwesomeIcon icon={faHtml5} className="fa-1x mx-2" />
                             <FontAwesomeIcon icon={faCss3} className="fa-1x mx-2" />
                             <FontAwesomeIcon icon={faReact} className="fa-1x mx-2" />
-                            <FontAwesomeIcon icon={faDatabase} className="fa-1x mx-2" />
                             <FontAwesomeIcon icon={faNode} className="fa-1x mx-2" />
                             <FontAwesomeIcon icon={faBootstrap} className="fa-1x mx-2" />
                             <FontAwesomeIcon icon={faStripe} className="fa-1x mx-2" />
                             <FontAwesomeIcon icon={faFigma} className="fa-1x mx-2" />
                             <FontAwesomeIcon icon={faGithub} className="fa-1x mx-2" />
+                            <i className="devicon-mysql-plain fa-1x mx-2"></i>
+                            <i className="devicon-mongodb-plain fa-1x mx-2"></i>
+                            <i className="devicon-sequelize-plain fa-1x mx-2"></i>
+                            <i className="devicon-express-original fa-1x mx-2"></i>
+                            <i className="devicon-bash-plain fa-1x mx-2"></i>
+                            <i className="devicon-blender-original fa-1x mx-2"></i>
+                            <i className="devicon-photoshop-plain fa-1x mx-2"></i>
+                            <i className="devicon-illustrator-plain fa-1x mx-2"></i>
+
+
+
+
+
                         </p>
                     </div>
                 </div>
@@ -34,7 +45,7 @@ function Description() {
                     <Col md={12} lg={6}>
                         <h4 className='about-me-title'>About Me</h4>
                         <p className="about-me-text">
-                            I'm Peyton Touma, a Full-Stack Web Developer based in Los Angeles, with roots in the small town of Olney, Maryland. Currently enrolled in the UCLA Full-Stack Web Development Bootcamp, I'll be completing the course in February 2024. My skill set includes JavaScript for dynamic web pages, HTML, CSS, and React for crafting engaging user interfaces. I work with Node.js and Express.js for building scalable server-side applications, and manage databases using MySQL, MongoDB, Sequelize, and Mongoose. Additionally, I'm proficient in front-end frameworks like Tailwind, Bootstrap, and Bulma, and use Webpack for optimized asset bundling as well as using the stripe api for e-commerce development. GitHub is my go-to for version control, ensuring efficient collaboration and code management.
+                        I'm Peyton Touma, a Full-Stack Web Developer based in Los Angeles, with roots in the small town of Olney, Maryland. Currently enrolled in the UCLA Full-Stack Web Development Bootcamp, I'll be completing the course in February 2024. My skill set includes JavaScript for dynamic web pages, HTML, CSS, and React for crafting engaging user interfaces. I work with Node.js and Express.js for building scalable server-side applications and manage databases using MySQL, MongoDB, Sequelize, and Mongoose. Additionally, I have a background in graphic design, utilizing tools such as Blender, Photoshop, Illustrator, and Cinema 4D. I am proficient in front-end frameworks like Tailwind, Bootstrap, and Bulma, and use Webpack for optimized asset bundling. Furthermore, I leverage the Stripe API for e-commerce development. GitHub is my go-to for version control, ensuring efficient collaboration and code management.
                         </p>
                     </Col>
                 </Row>
