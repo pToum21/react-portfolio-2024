e993419fe4dc352d3285d3f8f621b2cdfaac4170
toum21 <pey2moo1@gmail.com>
2024-01-13 14:58:13 -0800

    changed the view height of the portfolio component

--- client/src/components/Portfolio/Portfolio.jsx
+++ client/src/components/Portfolio/Portfolio.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import anyyes from '../../assets/anyyes.png';
-import { faGithub} from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const YourComponent = () => {
     return (
-        <Container>
+        <Container style={{ height: '100vh' }}>
             <Row>
                 <Col xs={12} md={6} className="d-flex align-items-center justify-content-center">
                     <img src={anyyes} alt="Your Image" className="img-fluid" />
@@ -23,14 +23,14 @@ const YourComponent = () => {
                             HTML, CSS (Bulma), Handlebars (with Express Session and Express Handlebars), JavaScript, Node.js, Express.js, MySQL (using mySQL2), Sequelize, Multer (for image upload), Blob storage, Stripe (for payments), bcrypt (for password hashing), dotenv, Config folder, Seeds, Splidejs (for image carousel).
                         </div>
                         <div className="d-flex justify-content-center">
-                            <a className="social-links" href="https://anyyes-3bf9b8d1cf29.herokuapp.com" style={{ fontSize: '0.8rem', color: 'white', textDecoration: 'none' }}>
-                                <p>Deployed Demo:</p>
-                                <FontAwesomeIcon icon={faArrowUpRightFromSquare} className="fa-2x mx-1" />
-                            </a>
-                            <a className="social-links" href="https://github.com/pToum21/anyYes" style={{ fontSize: '0.8rem', color: 'white', textDecoration: 'none' }}>
-                                <p>GitHub Repo:</p>
-                                <FontAwesomeIcon icon={faGithub} className="fa-2x mx-1" />
-                            </a>
+                            <div className="social-links mr-3">
+                                <p style={{ fontSize: '0.8rem', color: 'white', marginBottom: 0 }}>Deployed Demo:</p>
+                                <FontAwesomeIcon icon={faArrowUpRightFromSquare} className="fa-1x mx-1" />
+                            </div>
+                            <div className="social-links">
+                                <p style={{ fontSize: '0.8rem', color: 'white', marginBottom: 0 }}>GitHub Repo:</p>
+                                <FontAwesomeIcon icon={faGithub} className="fa-1x mx-1" />
+                            </div>
                         </div>
                     </div>
                 </Col>
@@ -40,3 +40,4 @@ const YourComponent = () => {
 };
 
 export default YourComponent;
+
