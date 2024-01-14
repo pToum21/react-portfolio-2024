6fb9dfb226ad300da63d7894321552cafa4b3cfa
toum21 <pey2moo1@gmail.com>
2024-01-13 21:41:59 -0800

    added a contact me component to app.jsx and to the file structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,10 @@ import Header from './components/Header/Header.jsx';
 import About from './components/Aboutme/About.jsx';
 import Portfolio from './components/Portfolio/Portfolio.jsx';
 import Description from './components/Description/Description.jsx';
-
+import Contact from './components/Contactme/Contact.jsx';
 
 import './styles.css';
+
 
 
 
@@ -19,6 +20,7 @@ function App() {
       <About />
       <Description />
       <Portfolio/>
+      <Contact />
     </>
   )
 }
---
+++ client/src/components/Contactme/Contact.jsx
@@ -0,0 +1,7 @@
+import './contact.css'
+
+function Contact() {
+
+}
+
+export default Contact;
