259e9c2e2f07bf27873d164a9d22e41756f871ca
toum21 <pey2moo1@gmail.com>
2024-01-14 13:57:53 -0800

    updated the position of the box

--- client/src/components/Contactme/Contact.jsx
+++ client/src/components/Contactme/Contact.jsx
@@ -1,48 +1,40 @@
-import './contact.css'
+// Contact.jsx
+import './contact.css';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
+import Button from 'react-bootstrap/Button';
 
 function Contact() {
     return (
-        <>
-            <InputGroup className="mb-3">
-                <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
-                <Form.Control
-                    placeholder="Username"
-                    aria-label="Username"
-                    aria-describedby="basic-addon1"
-                />
-            </InputGroup>
+        <div className="full-page">
+            <div className="contact-box">
+                <Form>
+                    <InputGroup className="mb-3">
+                        <Form.Control
+                            placeholder="Name/Company Name"
+                            aria-label="Name"
+                        />
+                    </InputGroup>
 
-            <InputGroup className="mb-3">
-                <Form.Control
-                    placeholder="Recipient's username"
-                    aria-label="Recipient's username"
-                    aria-describedby="basic-addon2"
-                />
-                <InputGroup.Text id="basic-addon2">@example.com</InputGroup.Text>
-            </InputGroup>
+                    <InputGroup className="mb-3">
+                        <Form.Control
+                            placeholder="Email"
+                            aria-label="Sender's Email"
+                            aria-describedby="basic-addon2"
+                        />
+                        <InputGroup.Text id="basic-addon2">@example.com</InputGroup.Text>
+                    </InputGroup>
 
-            <Form.Label htmlFor="basic-url">Your vanity URL</Form.Label>
-            <InputGroup className="mb-3">
-                <InputGroup.Text id="basic-addon3">
-                    https://example.com/users/
-                </InputGroup.Text>
-                <Form.Control id="basic-url" aria-describedby="basic-addon3" />
-            </InputGroup>
+                    <InputGroup>
+                        <InputGroup.Text>Message</InputGroup.Text>
+                        <Form.Control as="textarea" aria-label="With textarea" />
+                    </InputGroup>
 
-            <InputGroup className="mb-3">
-                <InputGroup.Text>$</InputGroup.Text>
-                <Form.Control aria-label="Amount (to the nearest dollar)" />
-                <InputGroup.Text>.00</InputGroup.Text>
-            </InputGroup>
-
-            <InputGroup>
-                <InputGroup.Text>With textarea</InputGroup.Text>
-                <Form.Control as="textarea" aria-label="With textarea" />
-            </InputGroup>
-        </>
-    )
+                    <Button as="input" type="submit" value="Submit" />
+                </Form>
+            </div>
+        </div>
+    );
 }
 
 export default Contact;
