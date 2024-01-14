a306561425a99fa0693410b8db7eb147f59d421a
toum21 <pey2moo1@gmail.com>
2024-01-13 17:46:15 -0800

    fixed responsive ness

--- client/src/components/Description/Description.jsx
+++ client/src/components/Description/Description.jsx
@@ -25,13 +25,13 @@ function Description() {
     const wireframeAnimation = useSpring({
         transform: `rotate(${scrollY / 5}deg)`,
     });
+
     return (
         <div className="description-container">
             <Container fluid className="description-content">
                 <div className="about-me-text">
                     <div className="mt-3 ">
                         <p className="tech-stack">
-                            My Tech Stack:
                             <FontAwesomeIcon icon={faJs} className="fa-1x mx-2" />
                             <FontAwesomeIcon icon={faHtml5} className="fa-1x mx-2" />
                             <FontAwesomeIcon icon={faCss3} className="fa-1x mx-2" />
@@ -49,18 +49,18 @@ function Description() {
                             <i className="devicon-blender-original fa-1x mx-2"></i>
                             <i className="devicon-photoshop-plain fa-1x mx-2"></i>
                             <i className="devicon-illustrator-plain fa-1x mx-2"></i>
-                        </p>
+                            </p>
                     </div>
                 </div>
                 <Row className="align-items-center">
-                    <Col md={12} lg={6} className='mb-4'>
+                    <Col xs={12} md={12} lg={6} className='mb-4'>
                         <animated.div style={wireframeAnimation} className="wireframe-polygon"></animated.div>
                         <img src={portrait} alt="Description Image" className="img-fluid description-image" />
                     </Col>
-                    <Col md={12} lg={6}>
+                    <Col xs={12} md={12} lg={6}>
                         <h4 className='about-me-title'>About Me</h4>
                         <p className="about-me-text">
-                        👋 I'm Peyton Touma, a Full-Stack Web Developer based in Los Angeles, with roots in the small town of Olney, Maryland. Currently enrolled in the UCLA Full-Stack Web Development Bootcamp, I'll be completing the course in February 2024. My skill set includes JavaScript for dynamic web pages, HTML, CSS, and React for crafting engaging user interfaces. I work with Node.js and Express.js for building scalable server-side applications and manage databases using MySQL, MongoDB, Sequelize, and Mongoose. Additionally, I have a background in graphic design, utilizing tools such as Blender, Photoshop, Illustrator, and Cinema 4D. I am proficient in front-end frameworks like Tailwind, Bootstrap, and Bulma, and use Webpack for optimized asset bundling. Furthermore, I leverage the Stripe API for e-commerce development. GitHub is my go-to for version control, ensuring efficient collaboration and code management.
+                            👋 I'm Peyton Touma, a Full-Stack Web Developer based in Los Angeles, with roots in the small town of Olney, Maryland. Currently enrolled in the UCLA Full-Stack Web Development Bootcamp, I'll be completing the course in February 2024. My skill set includes JavaScript for dynamic web pages, HTML, CSS, and React for crafting engaging user interfaces. I work with Node.js and Express.js for building scalable server-side applications and manage databases using MySQL, MongoDB, Sequelize, and Mongoose. Additionally, I have a background in graphic design, utilizing tools such as Blender, Photoshop, Illustrator, and Cinema 4D. I am proficient in front-end frameworks like Tailwind, Bootstrap, and Bulma, and use Webpack for optimized asset bundling. Furthermore, I leverage the Stripe API for e-commerce development. GitHub is my go-to for version control, ensuring efficient collaboration and code management.
                         </p>
                     </Col>
                 </Row>
