536f0dd7d228579d1b06e72a3c73c765a1141f58
toum21 <pey2moo1@gmail.com>
2024-01-14 13:39:54 -0800

    added bootstrap react form element

--- client/src/components/Contactme/Contact.jsx
+++ client/src/components/Contactme/Contact.jsx
@@ -1,38 +1,48 @@
 import './contact.css'
-import React from 'react';
-import { MDBInput, MDBCheckbox, MDBBtn, MDBValidation, MDBValidationItem, MDBTextArea } from 'mdb-react-ui-kit';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 
 function Contact() {
     return (
-        <MDBValidation noValidate id='form' className='text-center' style={{ width: '100%', maxWidth: '300px' }}>
-            <h2>Contact us</h2>
+        <>
+            <InputGroup className="mb-3">
+                <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+                <Form.Control
+                    placeholder="Username"
+                    aria-label="Username"
+                    aria-describedby="basic-addon1"
+                />
+            </InputGroup>
 
-            <MDBValidationItem invalid feedback='Please provide your name.'>
-                <MDBInput label='Name' v-model='name' wrapperClass='mb-4' required />
-            </MDBValidationItem>
+            <InputGroup className="mb-3">
+                <Form.Control
+                    placeholder="Recipient's username"
+                    aria-label="Recipient's username"
+                    aria-describedby="basic-addon2"
+                />
+                <InputGroup.Text id="basic-addon2">@example.com</InputGroup.Text>
+            </InputGroup>
 
-            <MDBValidationItem invalid feedback='Please provide your email.'>
-                <MDBInput type='email' label='Email address' v-model='email' wrapperClass='mb-4' required />
-            </MDBValidationItem>
+            <Form.Label htmlFor="basic-url">Your vanity URL</Form.Label>
+            <InputGroup className="mb-3">
+                <InputGroup.Text id="basic-addon3">
+                    https://example.com/users/
+                </InputGroup.Text>
+                <Form.Control id="basic-url" aria-describedby="basic-addon3" />
+            </InputGroup>
 
-            <MDBValidationItem invalid feedback='Please provide mail subject.'>
-                <MDBInput label='Subject' v-model='subject' wrapperClass='mb-4' required />
-            </MDBValidationItem>
+            <InputGroup className="mb-3">
+                <InputGroup.Text>$</InputGroup.Text>
+                <Form.Control aria-label="Amount (to the nearest dollar)" />
+                <InputGroup.Text>.00</InputGroup.Text>
+            </InputGroup>
 
-            <MDBValidationItem invalid feedback='Please provide a message text.'>
-                <MDBTextArea wrapperClass='mb-4' label='Message' required />
-            </MDBValidationItem>
-
-            <MDBValidationItem feedback=''>
-                <MDBCheckbox wrapperClass='d-flex justify-content-center' label='Send me copy' />
-            </MDBValidationItem>
-
-            <MDBBtn type='submit' color='primary' block className='my-4'>
-                Send
-            </MDBBtn>
-        </MDBValidation>
+            <InputGroup>
+                <InputGroup.Text>With textarea</InputGroup.Text>
+                <Form.Control as="textarea" aria-label="With textarea" />
+            </InputGroup>
+        </>
     )
-
 }
 
 export default Contact;
