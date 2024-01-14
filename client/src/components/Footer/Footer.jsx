81040d8e24be6890995c47eb265950baf8202f1b
toum21 <pey2moo1@gmail.com>
2024-01-14 15:47:51 -0800

    updated styling on footer component

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub, faDiscord } from '@fortawesome/free-brands-svg-icons';
+import './footer.css'
 
 const Footer = () => {
-  return (
-    <footer className="footer fixed-bottom py-3 bg-light text-center">
-      <div className="container">
-        <span className="text-muted">Your footer content goes here</span>
-      </div>
-    </footer>
-  );
+    return (
+        <footer className="footer fixed-bottom py-3 bg-dark">
+            <div className="container">
+                <a className="footer-links" href="https://www.linkedin.com/in/peyton-touma-b666a7281/">
+                    <FontAwesomeIcon icon={faLinkedin} className="fa-1x mx-1" />
+                </a>
+                <a className="footer-links" href="https://github.com/pToum21">
+                    <FontAwesomeIcon icon={faGithub} className="fa-1x mx-1" />
+                </a>
+                <a className="footer-links" href="https://discordapp.com/users/775148641014775808">
+                    <FontAwesomeIcon icon={faDiscord} className="fa-1x mx-1" />
+                </a>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
