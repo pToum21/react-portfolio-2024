5ea9a5f9f1ee32e52b01c2371f879fe6db0b36a5
toum21 <pey2moo1@gmail.com>
2024-01-14 22:36:01 -0800

    updated the footer to take similar styling to the links in the about me section

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -5,15 +5,15 @@ import './footer.css'
 const Footer = () => {
     return (
         <footer className="footer fixed-bottom py-3 bg-dark">
-            <div className="container">
+            <div className="footer-container">
                 <a className="footer-links" href="https://www.linkedin.com/in/peyton-touma-b666a7281/">
-                    <FontAwesomeIcon icon={faLinkedin} className="fa-1x mx-1" />
+                    <FontAwesomeIcon icon={faLinkedin} className="fa-1x mx-1 footer-icons" />
                 </a>
                 <a className="footer-links" href="https://github.com/pToum21">
-                    <FontAwesomeIcon icon={faGithub} className="fa-1x mx-1" />
+                    <FontAwesomeIcon icon={faGithub} className="fa-1x mx-1 footer-icons" />
                 </a>
                 <a className="footer-links" href="https://discordapp.com/users/775148641014775808">
-                    <FontAwesomeIcon icon={faDiscord} className="fa-1x mx-1" />
+                    <FontAwesomeIcon icon={faDiscord} className="fa-1x mx-1 footer-icons" />
                 </a>
             </div>
         </footer>
