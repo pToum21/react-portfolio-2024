827a66d09917be801a87f0236c74e82f6ce664ac
Peyton Touma <pey2moo1@gmail.com>
2024-07-31 13:35:09 -0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-
 ReactDOM.createRoot(document.getElementById('root')).render(
  <App />
 );
