036698672b18b3b12a347daeac43e02f4ac35be2
toum21 <pey2moo1@gmail.com>
2024-08-09 20:25:03 -0700

    updated portfolio

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -4,6 +4,7 @@ import './resume.css';
 import Button from 'react-bootstrap/Button';
 import { pdfjs } from 'react-pdf';
 import resumePdf from '../../assets/Peyton Touma Resume_8.7.24_1.pdf';
+
 function Resume() {
     const [numPages, setNumPages] = useState();
     const [pageNumber, setPageNumber] = useState(1);
