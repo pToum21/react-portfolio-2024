0028a2da7707d63b68c24d613ae5cb5f5f7d1112
toum21 <pey2moo1@gmail.com>
2024-08-18 15:47:25 -0700

    building the layout and style for the design page

--- src/components/DesignPortfolio/DesignPortfolio.jsx
+++ src/components/DesignPortfolio/DesignPortfolio.jsx
@@ -5,12 +5,12 @@ const DesignPortfolio = () => {
     return (
         <div>
             {/* Overlay for "Coming Soon" */}
-            <div className="overlay">
+            {/* <div className="overlay">
                 <div>
                     <h1>Under Construction</h1>
                     <p>Our design portfolio page is currently being updated. Please check back later!</p>
                 </div>
-            </div>
+            </div> */}
 
             <h1>Design Portfolio</h1>
         </div>
