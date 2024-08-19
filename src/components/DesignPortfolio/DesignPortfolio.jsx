29cf8b45e500d71a92cc6f83526abc94d1eb7f16
toum21 <pey2moo1@gmail.com>
2024-08-19 10:54:35 -0700

    working on adding an mp4

--- src/components/DesignPortfolio/DesignPortfolio.jsx
+++ src/components/DesignPortfolio/DesignPortfolio.jsx
@@ -5,8 +5,8 @@ import './design-portfolio.css';
 import cutePortals from '../../assets/design-projects/cute-portals/Monster Portals - Google Chrome 8_18_2024 4_14_56 PM.png';
 import blickyIo from '../../assets/design-projects/blicky.io/blik.png';
 import liber from '../../assets/design-projects/liber/lib.png';
-// import projectFour from './assets/images/project-four.jpg';
-// import projectFive from './assets/images/project-five.jpg';
+import Certain from '../../assets/design-projects/4Certain/4certain.png';
+import projectFive from './assets/images/project-five.jpg';
 // import projectSix from './assets/images/project-six.jpg';
 // import projectSeven from './assets/images/project-seven.jpg';
 // import projectEight from './assets/images/project-eight.jpg';
@@ -16,8 +16,8 @@ const DesignPortfolio = () => {
         { id: 1, name: 'Cute Portals', imageUrl: cutePortals, link: '/project-one' },
         { id: 2, name: 'Blicky.io', imageUrl: blickyIo, link: '/project-two' },
         { id: 3, name: 'Liber 3D', imageUrl: liber, link: '/project-three' },
-        // { id: 4, name: 'Project Four', imageUrl: projectFour, link: '/project-four' },
-        // { id: 5, name: 'Project Five', imageUrl: projectFive, link: '/project-five' },
+        { id: 4, name: '3D Face Scan', imageUrl: Certain, link: '/project-four' },
+        { id: 5, name: 'Project Five', imageUrl: projectFive, link: '/project-five' },
         // { id: 6, name: 'Project Six', imageUrl: projectSix, link: '/project-six' },
         // { id: 7, name: 'Project Seven', imageUrl: projectSeven, link: '/project-seven' },
         // { id: 8, name: 'Project Eight', imageUrl: projectEight, link: '/project-eight' },
