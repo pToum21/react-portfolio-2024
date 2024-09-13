272b61c3a85907f9254fa3cbcc935f5d4597d989
Peyton Touma <pey2moo1@gmail.com>
2024-09-12 17:22:45 -0700

    formatting

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -67,14 +67,10 @@ function Description() {
                         {/* about me description header title */}
                         <p className="about-me-text">
                         👋 Hi, I’m Peyton Touma, a Full-Stack Web Developer based in Los Angeles with roots in Olney, Maryland. I'm currently exploring new opportunities while working part-time as a Software Engineer at Impact Capitol DC, where I am developing the front end and UI for ALFReD AI, an advanced tool designed for the real estate industry.
-
-I recently completed the UCLA Full-Stack Web Development Bootcamp, where I honed my skills in JavaScript, HTML, CSS, and React to create dynamic and engaging web interfaces. My backend expertise includes Node.js, Express.js, Python, and Flask, with experience in managing databases using MySQL, SQLAlchemy, MongoDB, Sequelize, and Mongoose.
-
-In addition to my development skills, my background in graphic design is supported by tools like Blender, Photoshop, Illustrator, and Cinema 4D. I’m proficient in front-end frameworks such as Tailwind, Bootstrap, and Bulma, and I use Webpack for optimized asset bundling. I also work with the Stripe API for e-commerce solutions and rely on GitHub for version control and efficient code management.
-
-At Impact Capitol DC, I focus on developing intuitive and efficient user interfaces for ALFReD AI, enhancing its functionality for real estate professionals.
-
-If you’re seeking a dedicated and versatile web developer with a passion for innovation and a robust technical background, I’d love to connect!
+                            I recently completed the UCLA Full-Stack Web Development Bootcamp, where I honed my skills in JavaScript, HTML, CSS, and React to create dynamic and engaging web interfaces. My backend expertise includes Node.js, Express.js, Python, and Flask, with experience in managing databases using MySQL, SQLAlchemy, MongoDB, Sequelize, and Mongoose.
+                            In addition to my development skills, my background in graphic design is supported by tools like Blender, Photoshop, Illustrator, and Cinema 4D. I’m proficient in front-end frameworks such as Tailwind, Bootstrap, and Bulma, and I use Webpack for optimized asset bundling. I also work with the Stripe API for e-commerce solutions and rely on GitHub for version control and efficient code management.
+                            At Impact Capitol DC, I focus on developing intuitive and efficient user interfaces for ALFReD AI, enhancing its functionality for real estate professionals.
+                            If you’re seeking a dedicated and versatile web developer with a passion for innovation and a robust technical background, I’d love to connect!
                         </p>
                     </Col>
                 </Row>
